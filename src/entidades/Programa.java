701d7ef63ed232d36efda0227c4039ccd8030574
J.Junquera <javier.junquera.sanchez@gmail.com>
2017-06-09 01:28:32 +0200

    Implemented evaluation methods

--- src/entidades/Programa.java
+++ src/entidades/Programa.java
@@ -3,6 +3,7 @@ package entidades;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by junquera on 8/06/17.
@@ -10,7 +11,7 @@ import java.util.List;
 
 interface Statement {
 
-    public void execute(SymbolTable st);
+    public void execute(SymbolTable st) throws Exception;
 }
 
 
@@ -27,175 +28,238 @@ class IdentifierStatement implements Statement {
     }
 
     @Override
-    public void execute(SymbolTable st) {
-
+    public void execute(SymbolTable st) throws Exception {
+        st.set(i.getId(), i);
     }
 }
 
 class IfStatement implements Statement {
 
-    public IfStatement(Condition c, StatementList ifStmt) {
+    private Condition c;
+    private StatementList ifStmt;
+    private StatementList elseStmt;
 
+    public IfStatement(Condition c, StatementList ifStmt) {
+        this.c = c;
+        this.ifStmt = ifStmt;
+        this.elseStmt = new StatementList();
     }
 
     public IfStatement(Condition c, StatementList ifStmt, StatementList elseStmt) {
+        this.c = c;
+        this.ifStmt = ifStmt;
+        this.elseStmt = elseStmt;
 
     }
 
     @Override
-    public void execute(SymbolTable st) {
+    public void execute(SymbolTable st) throws Exception {
+        StatementList evaluated = c.getValue(st) ? ifStmt : elseStmt;
+
+        for (Statement s : evaluated.getList())
+            s.execute(st);
 
     }
 }
 
 class RepeatStatement implements Statement {
 
-    public RepeatStatement(Condition c, StatementList stmtList) {
+    private Condition c;
+    private StatementList stmtList;
 
+    public RepeatStatement(Condition c, StatementList stmtList) {
+        this.c = c;
+        this.stmtList = stmtList;
     }
 
     @Override
-    public void execute(SymbolTable st) {
-
+    public void execute(SymbolTable st) throws Exception {
+        while (c.getValue(st))
+            for (Statement s : this.stmtList.getList())
+                s.execute(st);
     }
 }
 
 class WriteStatement implements Statement {
 
-    public WriteStatement(Expression e) {
+    private Expression e;
 
+    public WriteStatement(Expression e) {
+        this.e = e;
     }
 
     @Override
-    public void execute(SymbolTable st) {
-
+    public void execute(SymbolTable st) throws Exception {
+        System.out.println(e.getValue(st));
     }
 }
 
 class ReadStatement implements Statement {
 
-    public ReadStatement(String i) {
+    private String id;
 
+    public ReadStatement(String id) {
+        this.id = id;
     }
 
     @Override
-    public void execute(SymbolTable st) {
+    public void execute(SymbolTable st) throws Exception {
+        String input = new Scanner(System.in).nextLine();
+        Identifier i = st.get(this.id);
 
+        if (i == null)
+            throw new Exception(String.format("Variable %s doesn't exist", this.id));
+
+        if (i instanceof IntegerIdentifier)
+            st.set(this.id, new IntegerIdentifier(this.id, Integer.parseInt(input)));
+        else if (i instanceof BooleanIdentifier)
+            st.set(this.id, new BooleanIdentifier(this.id, Boolean.parseBoolean(input)));
     }
 }
 
 
 interface Condition {
 
-    public boolean getValue(SymbolTable vl);
+    public boolean getValue(SymbolTable vl) throws Exception;
 
 }
 
 class NotCondition implements Condition {
 
-    public NotCondition(Condition c) {
+    private Condition c;
 
+    public NotCondition(Condition c) {
+        this.c = c;
     }
 
     @Override
-    public boolean getValue(SymbolTable vl) {
-        return false;
+    public boolean getValue(SymbolTable vl) throws Exception {
+        return !this.c.getValue(vl);
     }
 }
 
 class AndCondition implements Condition {
 
-    public AndCondition(Condition c1, Condition c2) {
+    private Condition c1;
+    private Condition c2;
 
+    public AndCondition(Condition c1, Condition c2) {
+        this.c1 = c1;
+        this.c2 = c2;
     }
 
     @Override
-    public boolean getValue(SymbolTable vl) {
-        return false;
+    public boolean getValue(SymbolTable vl) throws Exception {
+        return this.c1.getValue(vl) && this.c2.getValue(vl);
     }
 }
 
 class OrCondition implements Condition {
 
-    public OrCondition(Condition c1, Condition c2) {
+    private Condition c1;
+    private Condition c2;
 
+    public OrCondition(Condition c1, Condition c2) {
+        this.c1 = c1;
+        this.c2 = c2;
     }
 
     @Override
-    public boolean getValue(SymbolTable vl) {
-        return false;
+    public boolean getValue(SymbolTable vl) throws Exception {
+        return this.c1.getValue(vl) || this.c2.getValue(vl);
     }
 }
 
 class EqualCondition implements Condition {
+    private Expression e1;
+    private Expression e2;
 
     public EqualCondition(Expression e1, Expression e2) {
-
+        this.e1 = e1;
+        this.e2 = e2;
     }
 
     @Override
-    public boolean getValue(SymbolTable vl) {
-        return false;
+    public boolean getValue(SymbolTable vl) throws Exception {
+        return this.e1.getValue(vl) == this.e2.getValue(vl);
     }
 }
 
 class MinorEqualCondition implements Condition {
+    private Expression e1;
+    private Expression e2;
 
     public MinorEqualCondition(Expression e1, Expression e2) {
+        this.e1 = e1;
+        this.e2 = e2;
 
     }
 
     @Override
-    public boolean getValue(SymbolTable vl) {
-        return false;
+    public boolean getValue(SymbolTable vl) throws Exception {
+        return this.e1.getValue(vl) <= this.e2.getValue(vl);
     }
 }
 
 class MinorCondition implements Condition {
+    private Expression e1;
+    private Expression e2;
 
     public MinorCondition(Expression e1, Expression e2) {
+        this.e1 = e1;
+        this.e2 = e2;
 
     }
 
     @Override
-    public boolean getValue(SymbolTable vl) {
-        return false;
+    public boolean getValue(SymbolTable vl) throws Exception {
+        return this.e1.getValue(vl) < this.e2.getValue(vl);
     }
 }
 
 class MajorCondition implements Condition {
+    private Expression e1;
+    private Expression e2;
 
     public MajorCondition(Expression e1, Expression e2) {
+        this.e1 = e1;
+        this.e2 = e2;
 
     }
 
     @Override
-    public boolean getValue(SymbolTable vl) {
-        return false;
+    public boolean getValue(SymbolTable vl) throws Exception {
+        return this.e1.getValue(vl) > this.e2.getValue(vl);
     }
 }
 
 class MajorEqualCondition implements Condition {
+    private Expression e1;
+    private Expression e2;
 
     public MajorEqualCondition(Expression e1, Expression e2) {
+        this.e1 = e1;
+        this.e2 = e2;
 
     }
 
     @Override
-    public boolean getValue(SymbolTable vl) {
-        return false;
+    public boolean getValue(SymbolTable vl) throws Exception {
+        return this.e1.getValue(vl) >= this.e2.getValue(vl);
     }
 }
 
 class BooleanCondition implements Condition {
 
-    public BooleanCondition(boolean b) {
+    private boolean value;
 
+    public BooleanCondition(boolean value) {
+        this.value = value;
     }
 
     @Override
     public boolean getValue(SymbolTable vl) {
-        return false;
+        return this.value;
     }
 }
 
@@ -310,7 +374,7 @@ class Programa {
         this.stmtList = stmtList;
     }
 
-    public void test() {
+    public void test() throws Exception {
         List<Statement> rawStmntList = stmtList.getList();
 
         for (Statement s : rawStmntList) {
@@ -336,6 +400,17 @@ class SymbolTable {
     public Identifier get(String id) {
         return this.varList.get(id);
     }
+
+    public void set(String id, Identifier identifier) throws Exception {
+        Identifier i = this.varList.get(id);
+        if (i == null)
+            throw new Exception(String.format("Variable %s doesn't exist.", id));
+
+        if (identifier.getType() != i.getType())
+            throw new Exception(String.format("Type mismatch on %s assign.", id));
+
+        this.varList.replace(id, identifier);
+    }
 }
 
 class StatementList {
@@ -355,6 +430,9 @@ class StatementList {
 }
 
 interface Identifier {
+    public String getId();
+
+    public int getType();
 }
 
 class PseudoIdentifier implements Identifier {
@@ -366,6 +444,20 @@ class PseudoIdentifier implements Identifier {
     public PseudoIdentifier(String id) {
         this.id = id;
         this.value = id;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public int getType() {
+        return TYPE;
     }
 }
 
@@ -383,6 +475,16 @@ class IntegerIdentifier implements Identifier {
     public int getValue() {
         return this.value;
     }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public int getType() {
+        return TYPE;
+    }
 }
 
 class BooleanIdentifier implements Identifier {
@@ -398,6 +500,16 @@ class BooleanIdentifier implements Identifier {
 
     public boolean getValue() {
         return this.value;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public int getType() {
+        return TYPE;
     }
 }
 
