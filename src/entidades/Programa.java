c58f8c7912d7a74046cbfbe3e019235dcb8199d3
J.Junquera <javier.junquera.sanchez@gmail.com>
2017-06-09 00:13:47 +0200

    Más entidades

--- src/entidades/Programa.java
+++ src/entidades/Programa.java
@@ -11,11 +11,187 @@ import java.util.List;
 interface Statement {
 }
 
+
+class IdentifierStatement implements Statement {
+
+    private Identifier i;
+
+    public IdentifierStatement(String id, Expression e){
+        this.i = i;
+    }
+
+    public IdentifierStatement(String id, Condition c){
+        this.i = i;
+    }
+}
+
+class IfStatement implements Statement {
+
+    public IfStatement(Condition c, StatementList ifStmt){
+
+    }
+
+    public IfStatement(Condition c, StatementList ifStmt, StatementList elseStmt){
+
+    }
+
+}
+
+class RepeatStatement implements Statement {
+
+    public RepeatStatement(Condition c, StatementList stmtList){
+
+    }
+
+}
+
+class WriteStatement implements Statement {
+
+    public WriteStatement(Expression e){
+
+    }
+
+}
+
+class ReadStatement implements Statement {
+
+    public ReadStatement(String i){
+
+    }
+
+}
+
+
 interface Condition {
+}
+
+class NotCondition implements Condition {
+
+    public NotCondition(Condition c){
+
+    }
+
+}
+
+class AndCondition implements Condition {
+
+    public AndCondition(Condition c1, Condition c2){
+
+    }
+
+}
+
+class OrCondition implements Condition {
+
+    public OrCondition(Condition c1, Condition c2){
+
+    }
+
+}
+
+class EqualCondition implements Condition {
+
+    public EqualCondition(Expression e1, Expression e2){
+
+    }
+
+}
+
+class MinorEqualCondition implements Condition {
+
+    public MinorEqualCondition(Expression e1, Expression e2){
+
+    }
+
+}
+
+class MinorCondition implements Condition {
+
+    public MinorCondition(Expression e1, Expression e2){
+
+    }
+
+}
+
+class MajorCondition implements Condition {
+
+    public MajorCondition(Expression e1, Expression e2){
+
+    }
+
+}
+
+class MajorEqualCondition implements Condition {
+
+    public MajorEqualCondition(Expression e1, Expression e2){
+
+    }
+
+}
+
+class MinorMajorCondition implements Condition {
+
+    public MinorMajorCondition(Expression e1, Expression e2){
+
+    }
+
+}
+
+class BooleanCondition implements Condition {
+
+    public BooleanCondition(Boolean b){
+
+    }
+
 }
 
 interface Expression {
 }
+
+class SumaExpression implements Expression {
+
+    public SumaExpression(Expression e1, Expression e2){
+    }
+
+}
+
+class RestaExpression implements Expression {
+
+    public RestaExpression(Expression e1, Expression e2){
+    }
+
+}
+
+class MultiplicacionExpression implements Expression {
+
+    public MultiplicacionExpression(Expression e1, Expression e2){
+    }
+
+}
+
+class DivisionExpression implements Expression {
+
+    public DivisionExpression(Expression e1, Expression e2){
+    }
+
+}
+
+class IntegerExpression implements Expression {
+
+    public IntegerExpression(int i){
+    }
+
+}
+
+class IdentifierExpression implements Expression {
+
+    public IdentifierExpression(String id){
+    }
+
+}
+
+
+//-------------------------------------------------------------------------
 
 class Programa {
     VarList varList;
